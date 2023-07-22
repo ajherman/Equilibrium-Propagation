a2aaff0a337e3f5e8de4df37dbfa4ce603e9e151
Robert Russell Strauss - 358504 <rstrauss@darwin-fe1.lanl.gov>
2023-07-22 16:48:45 -0600

    uploaded script files for tests

---
+++ Laborieux-Arch/laborieux-cifar-mse-arch.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+python ../main.py \
+	--model 'CNN' \
+	--task 'CIFAR10' \
+	--channels 128 256 512 512 \
+	--kernels 3 3 3 3 \
+	--pools 'mmmm' \
+	--strides 1 1 1 1 \
+	--paddings 1 1 1 0 \
+	--fc 10 \
+	--optim 'sgd' \
+	--mmt 0.9 \
+	--lrs 0.25 0.15 0.1 0.08 0.05 \
+	--lr-decay \
+	--epochs 120 \
+	--wds 3e-4 3e-4 3e-4 3e-4 3e-4 \
+	--act 'mysig' \
+	--todo 'train' \
+	--betas 0.0 0.5 \
+	--thirdphase \
+	--T1 250 \
+	--T2 30 \
+	--mbs 128 \
+	--loss 'mse' \
+	--save  \
+	--device 0 \
+	--load-path results/EP/mse/2023-07-18/12-16-14_gpu0
